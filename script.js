888ca8f8f4294334ffd9add09e658fa3732707cb
Clubtowers <89275231+Clubtowers@users.noreply.github.com>
2021-11-10 15:07:45 -0600

    i

---
+++ script.js
@@ -0,0 +1,12 @@
+const d = new Date();
+let text = d.toLocaleDateString();
+
+function todayIs() {
+  if (text == "11/10/2021"){
+    document.getElementById('today').innerHTML = "National Hot Dog Day";
+  }else if(text == "11/1/2021"){
+    document.getElementById('today').innerHTML = "National Trampoline Day";
+  }else{
+    document.getElementById('today').innerHTML = "Today is not special";
+  }
+}
