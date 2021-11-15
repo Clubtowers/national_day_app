e03bb5771aabdb67960dfdb359ccd4ed856e4583
Clubtowers <89275231+Clubtowers@users.noreply.github.com>
2021-11-15 15:10:34 -0600

    1

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const d = new Date()
-const years = Date.prototype.getFullYear()
-let yearText = years.toLocaleDateString();
-let text = d.toLocaleDateString();
-if(text == "11/15/21")
-{
-  text = "11/15/"+yearText;
-}
+const months = d.getMonth()
+const days = d.getDay()
 
 
+
+console.log(months);
 
 function todayIs() {
-  if (text == "11/15/"+yearText){
+  if (months == 10 && days == 1){
     document.getElementById('today').innerHTML = "National Spicy Hermit Cookie Day";
-  }else if(text == "11/16/"+years){
-    document.getElementById('today').innerHTML = "National Vutton Day";
+  }else if(months == 10 && dayText == 2){
+    document.getElementById('today').innerHTML = "National Button Day";
   }else{
     document.getElementById('today').innerHTML = "Today is not special";
   }
 
 }
+//month[0] = "January";
+//month[1] = "February";
+//month[2] = "March";
+//month[3] = "April";
+//month[4] = "May";
+//month[5] = "June";
+//month[6] = "July";
+//month[7] = "August";
+//month[8] = "September";
+//month[9] = "October";
+//month[10] = "November";
+//month[11] = "December";
+// sunday = 0
+// monday = 1
+// tuesday = 2
+// wednesday = 3
+//thurday = 4
+//friday = 5
+// saturday = 6
+//ignore
